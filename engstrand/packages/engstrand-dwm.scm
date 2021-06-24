0c9432c9e5b3dbf482c1078e8eb9bcc6c4efb233
Fredrik Engstrand <fredrik@engstrand.nu>
2021-06-24 14:03:27 +0200

    fix: update incorrect package hashes

--- engstrand/packages/engstrand-dwm.scm
+++ engstrand/packages/engstrand-dwm.scm
@@ -15,7 +15,7 @@
           (url "https://github.com/engstrand-config/dwm.git")
           (commit "a68d74bf39da15b80e193eb25e251d341c179c6c")))
 	(sha256
-	  (base32 "0mk25nmqs9h0bd522ymnb6m6wq14m5vrwlzv24p31w9vc6qaxrh9"))))
+	  (base32 "1zy90y7y4mdpd05bbka2qmlvbbn66wvw96pkd772a5z7jjv9ka5h"))))
     (home-page "https://github.com/engstrand-config/dwm")
     (synopsis "Engstrand dwm")))
 
