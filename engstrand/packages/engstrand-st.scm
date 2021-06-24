7bcc8dbb3f46538f2335c62032fce7cfd2f9fc7a
Fredrik Engstrand <fredrik@engstrand.nu>
2021-06-24 17:30:33 +0200

    packages/st: update compile dependencies

--- engstrand/packages/engstrand-st.scm
+++ engstrand/packages/engstrand-st.scm
@@ -17,5 +17,11 @@
 		(sha256
 		    (base32 "097ifphqpb6v9zvmlh8v2nx3kgccwlgkfa1c9l21574kzsx1q1qq"))))
 	(home-page "https://github.com/engstrand-config/st")
+    	(inputs
+     	    `(("libx11" ,libx11)
+       	      ("libxft" ,libxft)
+	      ("fontconfig" ,fontconfig)
+	      ("freetype" ,freetype)
+	      ("harfbuzz" ,harfbuzz)))
 	(synopsis "Engstrand st")))
 
