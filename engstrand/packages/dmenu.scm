fb200c0b5fd78e26126142b4bfa120916e20aecc
Fredrik Engstrand <fredrik@engstrand.nu>
2021-06-24 13:04:45 +0200

    fix: add missing parenthesis is dmenu and st definitions

--- engstrand/packages/dmenu.scm
+++ engstrand/packages/dmenu.scm
@@ -13,7 +13,7 @@
 		(method git-fetch)
 		(uri (git-reference
 		    (url "https://github.com/engstrand-config/dmenu.git")
-		    (commit "eeb3448e0a76d55b75882bd633adf2b6a4ac2412")))
+		    (commit "eeb3448e0a76d55b75882bd633adf2b6a4ac2412")))))
 	(home-page "https://github.com/engstrand-config/dmenu")
 	(synopsis "Engstrand dmenu")))
 
--- engstrand/packages/st.scm
+++ engstrand/packages/st.scm
@@ -13,7 +13,7 @@
 		(method git-fetch)
 		(uri (git-reference
 		    (url "https://github.com/engstrand-config/st.git")
-		    (commit "0af4782a47cc1b0918bdc41fb61b1a5d358f75f6")))
+		    (commit "0af4782a47cc1b0918bdc41fb61b1a5d358f75f6")))))
 	(home-page "https://github.com/engstrand-config/st")
 	(synopsis "Engstrand st")))
 
