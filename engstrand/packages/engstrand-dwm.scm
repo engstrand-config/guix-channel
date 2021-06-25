60b6ee9c65f25e8c0c9a00d7c528774ce0aceead
Fredrik Engstrand <fredrik@engstrand.nu>
2021-06-25 23:49:14 +0200

    packages/dwm: update hash

--- engstrand/packages/engstrand-dwm.scm
+++ engstrand/packages/engstrand-dwm.scm
@@ -13,9 +13,9 @@
         (method git-fetch)
         (uri (git-reference
           (url "https://github.com/engstrand-config/dwm.git")
-          (commit "b8c72280b266ba16d8611223642f5caf68989b63")))
+          (commit "5f8207102e9039856499636d815b1bea7c7b83d6")))
 	(sha256
-	  (base32 "0w9cjv4lmq47anapghbbjfivkiap8yin8hrk26sz15ak6qmjwnjr"))))
+	  (base32 "0ph40alzkczbbs5wlkv4ph251kf96whmzah8d0hz0whndl99p292"))))
     (home-page "https://github.com/engstrand-config/dwm")
     (synopsis "Engstrand dwm")))
 
