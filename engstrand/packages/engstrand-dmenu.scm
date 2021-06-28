0fddb7e2029cd769de93aec5c60c9fa65764dd82
Johan Engstrand <johan@engstrand.nu>
2021-06-28 19:45:54 +0200

    fix: add commit, hash for dmenu

--- engstrand/packages/engstrand-dmenu.scm
+++ engstrand/packages/engstrand-dmenu.scm
@@ -35,10 +35,10 @@
 		(method git-fetch)
 		(uri (git-reference
 		    (url "https://github.com/engstrand-config/dmenu.git")
-		    (commit "")))
+		    (commit "eeb3448e0a76d55b75882bd633adf2b6a4ac2412")))
 		(sha256
-		  (base32 ""))))
-	(arguments 
+		  (base32 "0lyvrvrllq2k7y21n9hh1dn7vj9iv9682hynvs6kn71lcz0aakga"))))
+	(arguments
 	    `(#:validate-runpath? #f ; skip validation
      	      #:tests? #f            ; no tests
        	      #:make-flags
