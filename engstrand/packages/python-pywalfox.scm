3b85d28164f97f4cb42003e55c8a097c09ae01c3
Johan Engstrand <johan@engstrand.nu>
2021-06-29 12:40:47 +0200

    package: testing pywalfox from pypi

---
+++ engstrand/packages/python-pywalfox.scm
@@ -0,0 +1,23 @@
+(define-module (engstrand packages engstrand-dwm)
+  #:use-module (guix packages)
+  #:use-module (engstrand packages))
+
+((define-public python-pywalfox
+(package
+  (name "python-pywalfox")
+  (version "2.7.3")
+  (source
+    (origin
+      (method url-fetch)
+      (uri (pypi-uri "pywalfox" version))
+      (sha256
+        (base32
+          "1gkxcnysygvcpfhinaxaa6lf7b7194x7vi928i0qpdw962ck1zsi"))))
+  (build-system python-build-system)
+  (home-page
+    "https://github.com/frewacom/pywalfox")
+  (synopsis
+    "Native app used alongside the Pywalfox browser extension")
+  (description
+    "Native app used alongside the Pywalfox browser extension")
+  (license #f)))
