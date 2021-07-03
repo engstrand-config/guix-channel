3536ff720c77508e682c0c6247a3382cb3a0cb61
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-03 21:56:33 +0200

    packages/engstrand-dmenu: add missing build dependency

--- engstrand/packages/engstrand-dmenu.scm
+++ engstrand/packages/engstrand-dmenu.scm
@@ -2,6 +2,7 @@
     #:use-module (guix packages)
     #:use-module (guix download)
     #:use-module (guix git-download)
+    #:use-module (guix build-system gnu)
     #:use-module (gnu packages suckless)
     #:use-module (gnu packages xorg)
     #:use-module (engstrand packages)
