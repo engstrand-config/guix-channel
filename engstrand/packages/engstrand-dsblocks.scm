d419908f4ad2a702725e7325b686dfa8d5d6c3ca
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-03 22:28:37 +0200

    packages/engstrand-dsblocks: add missing make arguments

--- engstrand/packages/engstrand-dsblocks.scm
+++ engstrand/packages/engstrand-dsblocks.scm
@@ -1,7 +1,10 @@
 (define-module (engstrand packages engstrand-dsblocks)
+    #:use-module (guix utils)
     #:use-module (guix packages)
     #:use-module (guix download)
     #:use-module (guix git-download)
+    #:use-module (guix build-system gnu)
+    #:use-module (guix build-system glib-or-gtk)
     #:use-module (gnu packages)
     #:use-module (gnu packages fonts)
     #:use-module (gnu packages fontutils)
@@ -9,6 +12,7 @@
     #:use-module (gnu packages mpd)
     #:use-module (gnu packages xorg)
     #:use-module (gnu packages glib)
+    #:use-module (gnu packages pkg-config)
     #:use-module (engstrand packages)
     #:use-module (engstrand packages engstrand-dwm))
 
@@ -24,6 +28,18 @@
           (commit "404d436455f8ab1de058d7c94363556dbdada0d6")))
 	(sha256
 	  (base32 "1kb9an9bmfg9vms4jkz8z400ydrs72plz6rqaakrrfh3in6msjyc"))))
+    (arguments
+	`(#:tests? #f                      ; no tests
+	  #:make-flags
+          (list (string-append "CC=" ,(cc-for-target))
+               (string-append "PREFIX=" %output)
+               (string-append "FREETYPEINC="
+                            (assoc-ref %build-inputs "freetype")
+                            "/include/freetype2"))
+	  #:phases
+	  (modify-phases %standard-phases (delete 'configure))))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)))
     (inputs
 	`(("libx11" ,libx11)
 	  ("libxft" ,libxft)
